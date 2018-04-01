60e0f39cab54bc22934296eb2cef19f3d00593d3
Gabriel Sperb Stoffel <gsperbstoffel@gmail.com>
2018-04-01 19:47:56 -0300

    Criado a especialização para o médico

---
+++ src/main/java/hospital/models/Especializacao.java
@@ -0,0 +1,41 @@
+package hospital.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Especializacao", uniqueConstraints = @UniqueConstraint(columnNames = {"descricao"}))
+public class Especializacao {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(name = "descricao")
+    private String descricao;
+
+    public Especializacao() {
+    }
+
+    public Especializacao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
